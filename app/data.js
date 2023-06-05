3b38a31fd473b84b021441e40a38c393f24e9f38
Selim Sami <Selim.Genc@hhu.de>
2023-06-05 11:17:55 +0200

    finished project

---
+++ app/data.js
@@ -0,0 +1,68 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'What is the capital of Germany?',
+        answers: ['Madrid', 'Paris', 'Rome', 'Berlin'],
+        correctAnswer: 'Berlin',
+      },
+      {
+        id: 2,
+        question: 'What is the largest planet in our solar system?',
+        answers: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+        correctAnswer: 'Jupiter',
+      },
+      {
+        id: 3,
+        question: 'What is the smallest country in the world?',
+        answers: ['Monaco', 'Maldives', 'Vatican City', 'San Marino'],
+        correctAnswer: 'Vatican City',
+      },
+      {
+        id: 4,
+        question: 'What is the most widely spoken language in the world?',
+        answers: ['English', 'Mandarin', 'Spanish', 'Hindi'],
+        correctAnswer: 'Mandarin',
+      },
+      {
+        id: 5,
+        question: 'Who is the founder of Tesla?',
+        answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
+        correctAnswer: 'Elon Musk',
+      },
+      {
+        id:6,
+        question:"What country has the highest life expectancy? ",
+        answers:["Singapore","Germany","USA","Hong Kong"],
+        correctAnswer:"Hong Kong",
+      },
+      {
+        id:7,
+        question:"Who painted the Mona Lisa?",
+        answers:["Leonardo da Vinci","Michelangelo","Van Gogh","Clauda Monet"],
+        correctAnswer:"Leonardo da Vinci",
+      },
+      {
+        id:8,
+        question:"How many times has Germany won World Cup ?",
+        answers:["2","4","5","6"],
+        correctAnswer:"4",
+      },
+      {
+        id:9,
+        question:"What is the capital of New Zealand?",
+        answers:["Sydney","Mellbourne","Wellington","Washington"],
+        correctAnswer:"Wellington",
+      },
+      {
+        id:10,
+        question:"How many colors are there in a rainbow?",
+        answers:["4","5","6","7"],
+        correctAnswer:"7",
+      },
+      
+      
+    ],
+  };
+  
